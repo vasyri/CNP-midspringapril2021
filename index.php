0e16b7f0666ceb048531f69b7db8c3506ddc7ef5
vasyri <vasylenko99@gmail.com>
2021-05-13 14:26:40 +0300

    CNPmidspringapril2021

---
+++ index.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="shortcut icon" href="../img/favicon.png" type="image/x-icon">
+  <title>Clear Nail Plus</title>
+
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@500;600;700;800;900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="normalize.css">
+  <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+  <!-- header -->
+  <header class="header content-wrapper">
+    <div class="header__wrapper">
+      <div class="header__logo">
+        <img src="./img/header__logo.png" alt="header__logo" />
+      </div>
+      <nav class="header__nav">
+        <a href="#home">HOME</a>
+        <a href="#ingredients">INGREDIENTS</a>
+        <a href="#testimonials">TESTIMONIALS</a>
+        <a href="#faq">FAQ</a>&nbsp;<a href="#order">ORDER</a>
+      </nav>
+    </div>
+  </header>
+
+  <!-- banner  -->
+  <section class="banner">
+    <div class="banner__title">
+      <h1>Nature Is Coming <u>Back To Life</u>.<br> <span>Ready For It?</span> <mark>[Mega Sale]</mark>
+      </h1>
+    </div>
+    <div class="banner__inner">
+      <p>
+        A Limited, <u><b>Exclusive Offer</b></u> For<br>
+        <span>Previous <b>ClearNailsPlus</b> Customers!</span>
+      </p>
+      <div class="banner__text">
+        <p><b>Enjoy Spring</b> With </p>
+        <p>Clear, Healthy Nails </p>
+        <p>At Less Than <b><u>Half The Price</u>!</b></p>
+        <p><b>(Over 50% OFF)</b></p>
+      </div>
+      <p>Mid-Spring Sale Valid Until April 18th, 2021 11:59 pm</p>
+      <div class="banner__img">
+        <img src="./img/banner__img.png" alt="banner__img">
+      </div>
+    </div>
+  </section>
+
+
+  <div class="sidebar">
+    <p>
+      SCROLL DOWN AND GRAB YOUR<br class="br-1080px"> MID-SPRING FUNGUS FIGHTING PACKAGE…<br class="br-780px"> BEFORE REMAINING STOCKS RUN OUT
+    </p>
+  </div>
+
+  <!-- timer -->
+  <div class="timer">
+    <div class="timer__wrapper content-wrapper">
+      <div class="timer__row">
+        <div class="timer__days">
+          <p>0</p>
+          DAYS
+        </div>
+        <div class="timer__hours">
+          <p>0</p>
+          HOURS
+        </div>
+      </div>
+      <div class="timer__row">
+        <div class="timer__minutes">
+          <p>0</p>
+          MINUTES
+        </div>
+        <div class="timer__seconds">
+          <p>0</p>
+          SECONDS
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- text -->
+  <div class="text">
+    <div class="text__wrapper content-wrapper">
+      <p>
+        <b>Spring is the season of new beginnings and new promises...</b>
+      </p>
+      <p>
+        Fresh buds bloom, there’s a musky scent in the air…
+      </p>
+      <p>
+        ….animals awaken and the earth seems to come to life again.
+      </p>
+      <p>
+        <u>But can you really enjoy spring in all its glory when you’re not healthy?</u>
+      </p>
+      <p>
+        You can have all the money in the world and you may live in a tropical paradise…
+      </p>
+      <p>
+        ...but what good is it if nail fungus makes your life difficult?
+      </p>
+      <p>
+        If you feel too embarrassed to take off your shoes in public, <b><u>you’re living with a problem</u>.</b>
+      </p>
+      <p>And sadly, it’s not a problem that takes care of itself.</p>
+      <p><b><u>Nail fungus can be a symptom of a deep, underlying health issue.</u></b></p>
+      <p>
+        It clings on to you like a parasite and makes your nails look ugly, brittle, yellow, and unhealthy.
+      </p>
+      <p>And even spring can feel like the most depressing winter!</p>
+      <p>Clear Nails Plus helps support healthy, clear nails and acts as a robust buffer against fungus!</p>
+      <p>
+        A special, proprietary formula makes <b>Clear Nails Plus</b> your best bet <u>against ugly nail fungus</u>.
+      </p>
+      <p>But it works best when it’s used for months rather than weeks!</p>
+      <p>
+        Until the <b>18th of April</b>, you can refill your supply at a huge discount.
+      </p>
+      <p>
+        Please note that this is a special discount <b>ONLY</b> for <u>previous customers like you</u>.
+      </p>
+      <p>If you want to enjoy spring in all its glory…</p>
+      <p>If you want to free your feet from their ugly little prison…</p>
+      <p>
+        <b><u>This is easily your best chance!</u></b>
+      </p>
+      <p>
+        Please don’t wait for your fungus to grow worse…it can create problems in more ways than one.
+      </p>
+      <p>
+        Stock up on a 6-bottle package (with <u><b>FREE</b> shipping</u>) and save <b>51.4% instantly.</b>
+      </p>
+      <p>
+        Need another reason to invest in your health and vitality?
+      </p>
+      <p><b>Clear Nails Plus</b> comes with a <b><u>full refund policy</u></b>… in the unlikely case that you’re not happy with the <u>results.</u></p>
+      <p>
+        There will be no complicated forms to fill and no funny jargon.
+      </p>
+      <p><u>ZERO questions asked as long as you return the product within 6 months.</u></p>
+      <p>Sounds fair?</p>
+      <p>
+        But there’s one thing I can’t help you with... <b><u>and that’s time!</u></b>
+      </p>
+      <p>
+        You see… <b>Clear Nails Plus</b> has changed lives <u>all across America and the world</u>.
+      </p>
+      <p>Whenever I announce a sale, the demand is beyond crazy.</p>
+      <p><b><u> can’t promise you that I’ll have any inventory left within a day or two.</u></b></p>
+      <p>There’s no way I can match the capacity of the Big Pharma Industrial Complex.</p>
+      <p>I can’t keep up with the cheap assembly lines in India and China.</p>
+
+      <p>I work with a small All-American team with a modest capacity. We prefer <b><u>quality</u></b> over quantity.</p>
+      <p>
+        Once this batch is finished, it will be a <u>couple of months before we restock</u>.
+      </p>
+      <p>
+        And I can’t assure you there will be a sale of this magnitude!
+      </p>
+      <p><b>So please invest in the long-term safety of your nails and overall health</b>.</p>
+      <p>...and live without the shame of ugly nail fungus making life difficult for you.</p>
+      <p><b><u>Experience the joy of spring with the boundless energy and vitality of a kid.</u></b></p>
+      <div class="text__button-container button-desktop">
+        <img src="./img/text_button-img.png" alt="text__button" />
+      </div>
+      <div class="text__button-container button-mob">
+        <p>
+          *Click the button below to get access to the <b>51.4% Sale</b> instantly*
+        </p>
+        <img src="./img/text_button-img-mob.png" alt="button-mob" />
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
